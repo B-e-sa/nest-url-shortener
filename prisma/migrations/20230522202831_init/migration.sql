5eaa47cb340aa3d3802918ca6bfcf873cc055dbc
Besa <joaospereira963@gmail.com>
2023-05-22 17:32:01 -0300

    reset migrations, add hash logic

--- prisma/migrations/20230522202831_init/migration.sql
+++ prisma/migrations/20230522202831_init/migration.sql
@@ -2,7 +2,9 @@
 CREATE TABLE "users" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "email" TEXT NOT NULL,
-    "username" TEXT NOT NULL
+    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateTable
--- src/auth/auth/auth.controller.ts
+++ src/auth/auth/auth.controller.ts
@@ -1,14 +1,31 @@
-import { Controller, Post, HttpCode, HttpStatus, Req, Body } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) { }
+    constructor(
+        private authService: AuthService,
+        private prismaService: PrismaService
+    ) { }
 
     @Post('signup')
-    signup(@Body() dto: AuthDto) {
-        return this.authService.signup(dto)
+    async signup(@Body() dto: AuthDto) {
+        const { email, username } = dto;
+
+        const hash = await argon.hash(dto.password);
+
+        const user = await this.prismaService.user.create({
+            data: {
+                email,
+                username,
+                password: hash
+            }
+        });
+
+        return this.authService.signup(user)
     }
 
     @Post('signin')
