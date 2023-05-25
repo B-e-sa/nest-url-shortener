cb9c6bd0ae41e6b7753542d3fe69effd51b816bf
Besa <joaospereira963@gmail.com>
2023-05-25 17:26:49 -0300

    add jwt package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth/auth.module';
-import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 import { UrlModule } from './url/url.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule, UrlModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    UrlModule,
+    JwtModule.register({})
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
